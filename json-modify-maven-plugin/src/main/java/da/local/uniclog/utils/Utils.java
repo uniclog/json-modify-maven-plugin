42067e511808900e5bdbe80a014c31e66df1af31
birewallg <g.sstrav@gmail.com>
2023-08-22 20:16:52 +0300

    - add utils

--- json-modify-maven-plugin/src/main/java/da/local/uniclog/utils/Utils.java
+++ json-modify-maven-plugin/src/main/java/da/local/uniclog/utils/Utils.java
@@ -8,6 +8,7 @@ import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
 import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+import da.local.uniclog.execution.ExecutionType;
 import org.apache.maven.plugin.MojoExecutionException;
 
 import java.io.IOException;
@@ -15,9 +16,11 @@ import java.io.InputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.function.Function;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.isNull;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 public class Utils {
     private final Configuration configuration = Configuration.builder()
@@ -47,6 +50,39 @@ public class Utils {
             writer.writeValue(Paths.get(jsonOutputPath).toFile(), json.json());
         } catch (IOException e) {
             throw new MojoExecutionException("Unable to write file " + jsonOutputPath, e);
+        }
+    }
+
+    Object getElement(ExecutionType type, String value) throws MojoExecutionException {
+        switch (type) {
+            case BOOLEAN:
+                return getPrimitiveValue(value, Boolean::valueOf, ExecutionType.BOOLEAN);
+            case INTEGER:
+                return getPrimitiveValue(value, Integer::valueOf, ExecutionType.INTEGER);
+            case DOUBLE:
+                return getPrimitiveValue(value, Double::valueOf, ExecutionType.DOUBLE);
+            case JSON:
+                return getJsonValue(value);
+            case STRING:
+                return isNull(value) ? EMPTY : value;
+            default:
+                return value;
+        }
+    }
+
+    private <T> T getJsonValue(String value) throws MojoExecutionException {
+        DocumentContext valueAsJson = JsonPath.using(getConfiguration()).parse(value);
+        //log.debug(":: JSON: " + valueAsJson.jsonString());
+        return valueAsJson.json();
+    }
+
+    private <T> T getPrimitiveValue(String value, Function<String, T> fun, ExecutionType type) throws MojoExecutionException {
+        try {
+            return fun.apply(value);
+        } catch (NumberFormatException ex) {
+            String err = String.format("Convert format exception: %s -> %s", value, type.getValue());
+            //log.error(err);
+            throw new MojoExecutionException(err, ex);
         }
     }
 
--- json-modify-maven-plugin/src/main/java/da/local/uniclog/utils/UtilsInterface.java
+++ json-modify-maven-plugin/src/main/java/da/local/uniclog/utils/UtilsInterface.java
@@ -2,10 +2,15 @@ package da.local.uniclog.utils;
 
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.DocumentContext;
+import da.local.uniclog.execution.ExecutionType;
 import org.apache.maven.plugin.MojoExecutionException;
 
 public interface UtilsInterface {
     Utils utils = new Utils();
+
+    default Object getElement(ExecutionType type, String value) throws MojoExecutionException {
+        return utils.getElement(type, value);
+    }
 
     default DocumentContext readJsonObject(String jsonInputPath) throws MojoExecutionException {
         return utils.readJsonObject(jsonInputPath);
