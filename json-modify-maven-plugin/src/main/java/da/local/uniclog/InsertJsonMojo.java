3f6bc8c0a8b80fe7b46822d14067f9faa8e4d907
birewallg <g.sstrav@gmail.com>
2023-08-22 20:18:07 +0300

    - add insert mojo

--- json-modify-maven-plugin/src/main/java/da/local/uniclog/InsertJsonMojo.java
+++ json-modify-maven-plugin/src/main/java/da/local/uniclog/InsertJsonMojo.java
@@ -1,13 +1,15 @@
 package da.local.uniclog;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.JsonPathException;
 import da.local.uniclog.execution.ExecutionMojo;
+import da.local.uniclog.execution.ExecutionType;
 import da.local.uniclog.utils.UtilsInterface;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -19,7 +21,6 @@ import static java.util.Objects.nonNull;
 
 @Mojo(name = "insert", defaultPhase = LifecyclePhase.PREPARE_PACKAGE)
 public class InsertJsonMojo extends AbstractMojo implements UtilsInterface {
-    private Log log;
     @Parameter(alias = "json.in")
     private String jsonInputPath;
     @Parameter(alias = "json.out")
@@ -30,55 +31,68 @@ public class InsertJsonMojo extends AbstractMojo implements UtilsInterface {
     @Override
     public void execute() throws MojoExecutionException {
 
-         log.info(":: pr: " + jsonInputPath);
-         log.info(":: pr: " + jsonOutputPath);
-         executions.forEach(ex -> log.info(":: pr: " + ex.toString()));
+        getLog().info(":: pr: " + getJsonInputPath());
+        getLog().info(":: pr: " + getJsonOutputPath());
+        getExecutions().forEach(ex -> getLog().info(":: pr: " + ex.toString()));
 
+        DocumentContext json = readJsonObject(getJsonInputPath());
+        getLog().info(":: in: " + json.jsonString());
 
-        DocumentContext json = readJsonObject(jsonInputPath);
-        log.info(":: in: " + json.jsonString());
-
-        for (ExecutionMojo ex : executions) {
+        for (ExecutionMojo ex : getExecutions()) {
             try {
                 if (nonNull(ex.getValidation()) && validation(json, ex)) {
                     String err = String.format("Not valid element \"%s\" = %s", ex.getToken(), ex.getValidation());
-                    log.error(err);
+                    getLog().error(err);
                     throw new MojoExecutionException(err);
                 }
 
+                Object value = getElement(ex.getType(), ex.getValue());
 
                 JsonPath pathToArray = JsonPath.compile(ex.getToken());
-                log.info("pathToArray=" + pathToArray.getPath());
-                json.put(ex.getToken(), ex.getKey(), ex.getValue());
-                log.info(String.format(":: add -> %s : %s : %s", ex.getToken(), ex.getKey(), ex.getValue()));
+                getLog().info("pathToArray=" + pathToArray.getPath());
+
+                if (nonNull(ex.getKey())) {
+                    json.put(ex.getToken(), ex.getKey(), value);
+                } else if (ex.getType().equals(ExecutionType.JSON)) {
+                    ArrayNode array = json.read(ex.getToken());
+                    array.add((JsonNode) value);
+                    json.set(ex.getToken(), array);
+                } else {
+                    json.add(ex.getToken(), value);
+                }
+
+                getLog().info(String.format(":: add -> %s : %s : %s", ex.getToken(), ex.getKey(), ex.getValue()));
             } catch (JsonPathException e) {
                 String err = String.format("Not found json element \"%s\"", ex.getToken());
-                log.error(err);
+                getLog().error(err);
                 if (!ex.isSkipIfNotFoundElement()) {
                     throw new MojoExecutionException(err, e);
                 }
             }
         }
-
-        jsonOutputPath = isNull(jsonOutputPath) ? jsonInputPath : jsonOutputPath;
-        writeJsonObject(json, jsonOutputPath);
-        log.info(":: out: " + json.jsonString());
+        getLog().info("");
+        var out = isNull(getJsonOutputPath()) ? getJsonInputPath() : getJsonOutputPath();
+        writeJsonObject(json, out);
+        getLog().info(":: out: " + json.jsonString());
     }
 
-    @Override
-    public Log getLog() {
-        return log;
-    }
-
-    @Override
-    public void setLog(Log log) {
-        this.log = log;
-    }
 
     private boolean validation(DocumentContext json, ExecutionMojo ex) {
         Object object = json.read(ex.getToken());
         String node = object.toString();
-        log.info(String.format(":: validation: %s == %s", ex.getValidation(), node));
+        getLog().info(String.format(":: validation: %s == %s", ex.getValidation(), node));
         return !node.equals(ex.getValidation());
+    }
+
+    public String getJsonInputPath() {
+        return jsonInputPath;
+    }
+
+    public String getJsonOutputPath() {
+        return jsonOutputPath;
+    }
+
+    public List<ExecutionMojo> getExecutions() {
+        return executions;
     }
 }
