dd317f197b17a476c1a6e07cb6263cdd6388f393
birewallg <g.sstrav@gmail.com>
2023-08-23 17:05:42 +0300

    - add tests

--- json-modify-maven-plugin/src/test/java/da/local/uniclog/InsertJsonMojoTest.java
+++ json-modify-maven-plugin/src/test/java/da/local/uniclog/InsertJsonMojoTest.java
@@ -24,38 +24,62 @@ public class InsertJsonMojoTest {
 
     @Test
     public void testInsertValueKeyField() throws MojoExecutionException {
-        ExecutionMojo execution1 = getSpyExecutionMojo(
-                "@","key1", "value1",
-                ExecutionType.STRING, null,null);
-        ExecutionMojo execution2 = getSpyExecutionMojo(
-                "$.jo","key2", "value2",
-                ExecutionType.STRING, null,null);
-        ExecutionMojo execution3 = getSpyExecutionMojo(
-                "$.l1[2]","key3", "value3",
-                ExecutionType.STRING, null,null);
+        var execution1 = getSpyExecutionMojo(
+                "@", "key1", "value1",
+                ExecutionType.STRING, null, null, null);
+        var execution2 = getSpyExecutionMojo(
+                "$.jo", "key2", "value2",
+                ExecutionType.STRING, null, null, null);
+        var execution3 = getSpyExecutionMojo(
+                "$.l1[2]", "key3", "value3",
+                ExecutionType.STRING, null, null, null);
 
-        List<ExecutionMojo> executions = List.of(execution1, execution2, execution3);
+        var executions = List.of(execution1, execution2, execution3);
         doReturn(executions).when(service).getExecutions();
         service.execute();
     }
 
     @Test
     public void testInsertNewArrayItem() throws MojoExecutionException {
-        ExecutionMojo execution = getSpyExecutionMojo(
-                "$.l1",null, "{\"a1\": \"a2\"}",
-                ExecutionType.JSON, null,null);
-        List<ExecutionMojo> executions = List.of(execution);
+        var executions = List.of(
+                getSpyExecutionMojo(
+                        "$.l1", null, "{\"a1\": \"a2\"}",
+                        ExecutionType.JSON, null, null, null),
+                getSpyExecutionMojo(
+                        "$.l1", null, "value1",
+                        ExecutionType.STRING, null, null, null),
+                getSpyExecutionMojo(
+                        "$.l1", null, null,
+                        ExecutionType.NULL, null, null, null),
+                getSpyExecutionMojo(
+                        "$.l1[1]", "i1", "test4",
+                        ExecutionType.STRING, "{\"i1\":\"test2\"}", null, null)
+
+        );
         doReturn(executions).when(service).getExecutions();
         service.execute();
     }
 
-    private ExecutionMojo getSpyExecutionMojo(String token, String key, String value, ExecutionType type, String validation, Boolean skipIfNotFound) {
+    @Test
+    public void testInsertNewArrayItemToIndex() throws MojoExecutionException {
+        var executions = List.of(
+                getSpyExecutionMojo(
+                        "$.l1", null, "{\"l2\": \"test5\"}",
+                        ExecutionType.JSON, null, null, 1)
+        );
+        doReturn(executions).when(service).getExecutions();
+        service.execute();
+    }
+
+
+    private ExecutionMojo getSpyExecutionMojo(String token, String key, String value, ExecutionType type, String validation, Boolean skipIfNotFound, Integer arrayIndex) {
         ExecutionMojo execution = spy(new ExecutionMojo());
         doReturn(token).when(execution).getToken();
         doReturn(key).when(execution).getKey();
         doReturn(value).when(execution).getValue();
         doReturn(type).when(execution).getType();
         doReturn(validation).when(execution).getValidation();
+        doReturn(arrayIndex).when(execution).getArrayIndex();
         doReturn(!Objects.isNull(skipIfNotFound)).when(execution).isSkipIfNotFoundElement();
         return execution;
     }
