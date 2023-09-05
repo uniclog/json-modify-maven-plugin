7e015d87e71b42b52802173aacfa2e6590409367
birewallg <g.sstrav@gmail.com>
2023-09-05 12:30:24 +0300

    - add logger interface

---
+++ json-modify-maven-plugin/src/main/java/da/local/uniclog/utils/JmLogger.java
@@ -0,0 +1,19 @@
+package da.local.uniclog.utils;
+
+import org.apache.maven.plugin.logging.Log;
+
+public interface JmLogger {
+    Log getLogger();
+
+    default void info(String log) {
+        getLogger().info(log);
+    }
+
+    default void debug(String log) {
+        getLogger().debug(log);
+    }
+
+    default void error(String log) {
+        getLogger().error(log);
+    }
+}
