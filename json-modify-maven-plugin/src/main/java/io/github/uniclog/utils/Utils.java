f5bb6208a07803eba7009590f1b0f3a96fe795b7
birewallg <g.sstrav@gmail.com>
2023-10-03 22:26:29 +0300

    - fix logger

--- json-modify-maven-plugin/src/main/java/io/github/uniclog/utils/Utils.java
+++ json-modify-maven-plugin/src/main/java/io/github/uniclog/utils/Utils.java
@@ -88,7 +88,7 @@ public class Utils {
     boolean validation(DocumentContext json, ExecutionMojo ex, Integer exIndex, Log log) {
         Object object = json.read(ex.getToken());
         String node = object.toString();
-        log.info(String.format(":%d: validation: %s == %s", exIndex, ex.getValidation(), node));
+        log.info(String.format("(%d) :: validation: %s == %s", exIndex, ex.getValidation(), node));
         return !node.equals(ex.getValidation());
     }
 
